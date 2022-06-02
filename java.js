b30ffef9ba29bc68268998000767cd7e6d0a5e0e
Wasaaif <wasaaif.1998@hotmail.com>
2022-06-02 11:02:15 +0300

    initial commit

---
+++ java.js
@@ -0,0 +1,18 @@
+
+
+
+
+function Logout(element) {
+    element.innerHTML = "Logout";
+}
+
+function ert(element) {
+
+alert("edit your profil");
+
+}
+function remove(element) {
+
+    element.innerHTML = "";
+    
+    }
